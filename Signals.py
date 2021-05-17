2f58d12c2146a4f3050af5b376eeabc3bf769b75
kries <kries2333@gmail.com>
2021-05-17 16:40:11 +0800

    first commit

---
+++ Signals.py
@@ -0,0 +1,259 @@
+"""
+《邢不行-2020新版|Python数字货币量化投资课程》
+无需编程基础，助教答疑服务，专属策略网站，一旦加入，永续更新。
+课程详细介绍：https://quantclass.cn/crypto/class
+邢不行微信: xbx9025
+本程序作者: 邢不行
+
+# 课程内容
+币安u本位择时策略实盘框架需要的signal
+"""
+import pandas as pd
+import random
+import numpy as np
+import talib
+
+
+# 将None作为信号返回
+def real_signal_none(df, now_pos, avg_price, para):
+    """
+    发出空交易信号
+    :param df:
+    :param now_pos:
+    :param avg_price:
+    :param para:
+    :return:
+    """
+
+    return None
+
+
+# 随机生成交易信号
+def real_signal_random(df, now_pos, avg_price, para):
+    """
+    随机发出交易信号
+    :param df:
+    :param now_pos:
+    :param avg_price:
+    :param para:
+    :return:
+    """
+
+    r = random.random()
+    if r <= 0.25:
+        return 0
+    elif r <= 0.5:
+        return 1
+    elif r <= 0.75:
+        return -1
+    else:
+        return None
+
+
+# 布林策略实盘交易信号
+def real_signal_simple_bolling(df, now_pos, avg_price, para=[200, 2]):
+    """
+    实盘产生布林线策略信号的函数，和历史回测函数相比，计算速度更快。
+    布林线中轨：n天收盘价的移动平均线
+    布林线上轨：n天收盘价的移动平均线 + m * n天收盘价的标准差
+    布林线上轨：n天收盘价的移动平均线 - m * n天收盘价的标准差
+    当收盘价由下向上穿过上轨的时候，做多；然后由上向下穿过中轨的时候，平仓。
+    当收盘价由上向下穿过下轨的时候，做空；然后由下向上穿过中轨的时候，平仓。
+    :param df:  原始数据
+    :param para:  参数，[n, m]
+    :return:
+    """
+
+    # ===策略参数
+    # n代表取平均线和标准差的参数
+    # m代表标准差的倍数
+    n = int(para[0])
+    m = para[1]
+
+    # ===计算指标
+    # 计算均线
+    df['median'] = df['close'].rolling(n).mean()  # 此处只计算最后几行的均线值，因为没有加min_period参数
+    median = df.iloc[-1]['median']
+    median2 = df.iloc[-2]['median']
+    # 计算标准差
+    df['std'] = df['close'].rolling(n).std(ddof=0)  # ddof代表标准差自由度，只计算最后几行的均线值，因为没有加min_period参数
+    std = df.iloc[-1]['std']
+    std2 = df.iloc[-2]['std']
+    # 计算上轨、下轨道
+    upper = median + m * std
+    lower = median - m * std
+    upper2 = median2 + m * std2
+    lower2 = median2 - m * std2
+
+    # ===寻找交易信号
+    signal = None
+    close = df.iloc[-1]['close']
+    close2 = df.iloc[-2]['close']
+    # 找出做多信号
+    if (close > upper) and (close2 <= upper2):
+        signal = 1
+    # 找出做空信号
+    elif (close < lower) and (close2 >= lower2):
+        signal = -1
+    # 找出做多平仓信号
+    elif (close < median) and (close2 >= median2):
+        signal = 0
+    # 找出做空平仓信号
+    elif (close > median) and (close2 <= median2):
+        signal = 0
+
+    return signal
+
+# 闪闪的策略
+def real_signal_shanshan_bolling(df, now_pos, avg_price, para=[200, 2]):
+    """
+    实盘产生布林线策略信号的函数，和历史回测函数相比，计算速度更快。
+    布林线中轨：n天收盘价的移动平均线
+    布林线上轨：n天收盘价的移动平均线 + m * n天收盘价的标准差
+    布林线上轨：n天收盘价的移动平均线 - m * n天收盘价的标准差
+    当收盘价由下向上穿过上轨的时候，做多；然后由上向下穿过中轨的时候，平仓。
+    当收盘价由上向下穿过下轨的时候，做空；然后由下向上穿过中轨的时候，平仓。
+    :param df:  原始数据
+    :param para:  参数，[n, m]
+    :return:
+    """
+
+    # ===策略参数
+    # n代表取平均线和标准差的参数
+    # m代表标准差的倍数
+    bolling_window = int(para[0])
+    d = para[1]
+
+    # ===计算指标
+    # 计算均线
+    df['median'] = df['close'].rolling(bolling_window).mean()  # 此处只计算最后几行的均线值，因为没有加min_period参数
+    median = df.iloc[-1]['median']
+    median2 = df.iloc[-2]['median']
+
+    # 计算标准差
+    df['std'] = df['close'].rolling(bolling_window).std(ddof=0)  # ddof代表标准差自由度，只计算最后几行的均线值，因为没有加min_period参数
+    std = df.iloc[-1]['std']
+    std2 = df.iloc[-2]['std']
+
+    m = (abs((df['close'] - df['median']) / df['std'])).rolling(bolling_window, min_periods=1).max()
+    # 计算上轨、下轨道
+    upper = median + m.iloc[-1] * std
+    lower = median - m.iloc[-1] * std
+    upper2 = median2 + m.iloc[-2] * std2
+    lower2 = median2 - m.iloc[-2] * std2
+
+    # ===计算KDJ指标
+    df['k'], df['d'] = np.float16(talib.STOCH(high = df['high'], low = df['low'], close = df['close'],
+                                #此处三个周期(9,3,3)只是习惯取法可以作为参数更改
+                                fastk_period = 9, # RSV值周期
+                                slowk_period = 3, # 'K'线周期
+                                slowd_period = 3, # 'D'线周期
+                                slowk_matype = 1,  # 'K'线平滑方式，1为指数加权平均，0 为普通平均
+                                slowd_matype = 1))# 'D'线平滑方式，1为指数加权平均，0 为普通平均
+    df['j'] = df['k'] * 3 - df['d'] * 2   # 'J' 线
+
+    # 计算j值在kdj_window下的历史百分位
+    kdj_window = 9 # 此处的回溯周期取为上面计算kdj的fastk_period
+    j_max = df['j'].rolling(kdj_window, min_periods=1).max()
+    j_min = df['j'].rolling(kdj_window, min_periods=1).min()
+    df['j_exceed'] = abs(j_max - df['j']) / abs(j_max - j_min)
+    j_exceed = df.iloc[-1]['j_exceed']
+
+    # 计算ATR
+    df['atr'] = talib.ATR(high=df['high'],low=df['low'],close=df['close'],timeperiod=int(bolling_window/d))
+
+    # 计算ATR布林通道上下轨
+    df['atr_upper'] = df['median'] + df['atr']
+    df['atr_lower'] = df['median'] - df['atr']
+
+    atr_upper = df.iloc[-1]['atr_upper']
+    atr_lower = df.iloc[-1]['atr_lower']
+
+    # ===寻找交易信号
+    signal = None
+    close = df.iloc[-1]['close']
+    close2 = df.iloc[-2]['close']
+    # 找出做多信号
+    if (close > upper) and (close2 <= upper2) and (j_exceed <= 0.3) and (close >= atr_upper):
+        signal = 1
+    # 找出做空信号
+    elif (close < lower) and (close2 >= lower2) and (j_exceed >= 0.7) and (close <= atr_lower):
+        signal = -1
+    # 找出做多平仓信号
+    elif (close < median) and (close2 >= median2):
+        signal = 0
+    # 找出做空平仓信号
+    elif (close > median) and (close2 <= median2):
+        signal = 0
+
+    return signal
+
+def real_signal_adp2boll_v2(df, now_pos, avg_price, para=[200]):
+    """
+      实盘产生布林线策略信号的函数，和历史回测函数相比，计算速度更快。
+      布林线中轨：n天收盘价的移动平均线
+      布林线上轨：n天收盘价的移动平均线 + m * n天收盘价的标准差
+      布林线上轨：n天收盘价的移动平均线 - m * n天收盘价的标准差
+      当收盘价由下向上穿过上轨的时候，做多；然后由上向下穿过中轨的时候，平仓。
+      当收盘价由上向下穿过下轨的时候，做空；然后由下向上穿过中轨的时候，平仓。
+      :param df:  原始数据
+      :param para:  参数，[n, m]
+      :return:
+      """
+
+    # ===策略参数
+    # n代表取平均线和标准差的参数
+    # m代表标准差的倍数
+    n = int(para[0])
+
+    # ===计算指标
+    # 计算均线
+    df['median'] = df['close'].rolling(n, min_periods=1).mean()  # 此处只计算最后几行的均线值，因为没有加min_period参数
+    # 计算标准差
+    df['std'] = df['close'].rolling(n, min_periods=1).std(ddof=0)  # ddof代表标准差自由度，只计算最后几行的均线值，因为没有加min_period参数
+    df['z'] = abs(df['close'] - df['median']) / df['std']
+    df['z_score'] = df['z'].rolling(window=int(n/10)).mean()  # 先对z求n/10个窗口的平均值
+    df['m1'] = df['z_score'].rolling(window=n).max().shift(1)  # 再用n个窗口内z_score的最大值当作母布林带的m
+    df['m2'] = df['z_score'].rolling(window=n).min().shift(1)  # 再用n个窗口内z_score的最小值当作子布林带的m
+    df['upper'] = df['median'] + df['std'] * df['m1']
+    df['lower'] = df['median'] - df['std'] * df['m1']
+    df['up'] = df['median'] + df['std'] * df['m2']
+    df['dn'] = df['median'] - df['std'] * df['m2']
+
+    upper1 = df.iloc[-1]['upper']
+    upper2 = df.iloc[-2]['upper']
+
+
+    lower1 = df.iloc[-1]['lower']
+    lower2 = df.iloc[-2]['lower']
+
+    up1 = df.iloc[-1]['up']
+    up2 = df.iloc[-2]['up']
+
+    dn1 = df.iloc[-1]['dn']
+    dn2 = df.iloc[-2]['dn']
+
+    # ===寻找交易信号
+    signal = None
+    close1 = df.iloc[-1]['close']
+    close2 = df.iloc[-2]['close']
+
+
+    # 找出做多信号
+    if (close1 > upper1) and (close2 <= upper2):
+        signal = 1
+    # 找出做空信号
+    elif (close1 < lower1) and (close2 >= lower2):
+        signal = -1
+    # 找出做多平仓信号
+    elif (close1 < up1) and (close2 >= up2):
+        signal = 0
+    # 找出做空平仓信号
+    elif (close1 > dn1) and (close2 <= dn2):
+        signal = 0
+
+    print("time={}".format(df.iloc[-1]['candle_begin_time_GMT8']))
+    print("close={} upper={} lower={}".format(close1, upper1, lower1))
+    print("close2={} upper2={} lower2={}".format(close2, upper2, lower2))
+    print("signal={}  up1={} up2={} dn1={} dn2={}".format(signal, up1, up2, dn1, dn2))
+    return signal
---
+++ main.py
@@ -0,0 +1,141 @@
+"""
+《邢不行-2020新版|Python数字货币量化投资课程》
+无需编程基础，助教答疑服务，专属策略网站，一旦加入，永续更新。
+课程详细介绍：https://quantclass.cn/crypto/class
+邢不行微信: xbx9025
+本程序作者: 邢不行
+
+# 课程内容
+币安u本位择时策略实盘框架
+"""
+import pandas as pd
+import ccxt
+from Function import *
+from Config import *
+pd.set_option('display.max_rows', 1000)
+pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
+pd.set_option('display.unicode.ambiguous_as_wide', True)  # 设置命令行输出时的列对齐功能
+pd.set_option('display.unicode.east_asian_width', True)
+
+
+# ==========配置运行相关参数==========
+# =k线周期
+time_interval = '15m'  # 目前支持5m，15m，30m，1h，2h等。得交易所支持的K线才行。最好不要低于5m
+# =每次获取的K线数量
+recent_candle_num = 600
+
+# =交易所配置
+BINANCE_CONFIG = {
+    'apiKey': 'j4MI0GEdLGS6Aa6MlhuuBZqtGJBbSQRTVQuBKC9enPtGEGfol67jMFIi9Hkudno4',
+    'secret': 'gWSDCYJxQQKo174FEHvxTkzTcxFyzK4mlqXa3s13yslttRrZfkHnWgDoDNndZB1R',
+    'timeout': exchange_timeout,
+    'rateLimit': 10,
+    'verbose': False,
+    'hostname': 'fapi.binance.com',
+    'enableRateLimit': False}
+exchange = ccxt.binance(BINANCE_CONFIG)  # 交易所api
+
+
+"""
+闪闪的
+id = '574328f20962b37a14cf8a25b1b3f2710ba84d915203b3d912a8120bba2c29ce'
+secret = 'SEC6a978202c11e2d35b1143473cc435b4505009ac6b1a6260a11edeaff60ff4530'
+robot_id_secret = [robot_id, secret]
+"""
+
+"""
+kries
+robot_id = 03f3c6bd782651e92f7d53efa435b3716264e2d3d7cd409be5059a2147f973fd
+secret = SEC7ec69478e60a2bf4fda54879ee1acd7fb892cc6d2ddabc367d8c13f4b6e957d1
+"""
+
+# =钉钉
+# 在一个钉钉群中，可以创建多个钉钉机器人。
+# 建议单独建立一个报错机器人，该机器人专门发报错信息。请务必将报错机器人在id和secret放到function.send_dingding_msg的默认参数中。
+robot_id = '03f3c6bd782651e92f7d53efa435b3716264e2d3d7cd409be5059a2147f973fd'
+secret = 'SEC7ec69478e60a2bf4fda54879ee1acd7fb892cc6d2ddabc367d8c13f4b6e957d1'
+robot_id_secret = [robot_id, secret]
+
+
+# ==========配置策略相关参数==========
+# =symbol_config，更新需要交易的合约、策略参数、下单量等配置信息。主键为u本位合约的symbol。比特币永续为BTCUSDT，交割为BTCUSDT_210625
+symbol_config = {
+    'ETHUSDT': {'leverage': 2,  # 控制实际交易的杠杆倍数，在实际交易中可以自己修改。此处杠杆数，必须小于页面上的最大杠杆数限制
+                 'strategy_name': 'real_signal_adp2boll_v2',  # 使用的策略的名称
+                 'para': [905],  # 策略参数
+                 'position': 0.2,  # 该币种在总体资金中占比，几个币种相加要小于1
+                 }
+}
+
+# =获取交易精度
+usdt_future_exchange_info(exchange, symbol_config)
+
+
+def main():
+    # =判断是否单向持仓，若不是程序退出
+    if_oneway_mode(exchange)
+
+    # ==========获取需要交易币种的历史数据==========
+
+    # =进入每次的循环
+    while True:
+        # ==========获取持仓数据==========
+        # 初始化symbol_info，在每次循环开始时都初始化，防止上次循环的内容污染本次循环的内容。
+        symbol_info_columns = ['账户权益', '分配比例', '分配资金', '持仓方向', '持仓量', '持仓收益', '持仓均价', '当前价格']
+        symbol_info = pd.DataFrame(index=symbol_config.keys(), columns=symbol_info_columns)  # 转化为dataframe
+        symbol_info['分配比例'] = pd.DataFrame(symbol_config).T['position']
+
+        # 更新账户信息symbol_info
+        symbol_info = binance_update_account(exchange, symbol_config, symbol_info)
+        print('持仓信息\n', symbol_info)
+
+        # ==========根据当前时间，获取策略下次执行时间，例如16:15。并sleep至该时间==========
+        run_time = sleep_until_run_time(time_interval, if_sleep=True)
+
+        # ==========获取最新的k线数据==========
+        exchange.timeout = 1000  # 即将获取最新数据，临时将timeout设置为1s，加快获取数据速度
+        # 获取数据
+        symbol_candle_data = single_threading_get_binance_candle_data(exchange, symbol_config, symbol_info,
+                                                                      time_interval, run_time, recent_candle_num)
+        # 将最近的数据打印出
+        for symbol in symbol_config.keys():
+            print(symbol_candle_data[symbol].tail(min(2, recent_candle_num)))
+
+        # ==========计算每个币种的交易信号==========
+        symbol_signal = calculate_signal(symbol_info, symbol_config, symbol_candle_data)
+        print('\n产生信号时间:\n', symbol_info[['当前价格', '持仓方向', '目标持仓', '信号时间']])
+        print('\n本周期交易计划:', symbol_signal)
+
+        # ==========下单==========
+        exchange.timeout = exchange_timeout  # 下单时需要增加timeout的时间，将timout恢复正常
+        # 计算下单信息
+        symbol_order_params = cal_all_order_info(symbol_signal, symbol_info, symbol_config, exchange)
+        print('\n订单参数\n', symbol_order_params)
+
+        order_list = []
+        # 开始批量下单
+        num = 5  # 批量下单的数量
+        for i in range(0, len(symbol_order_params), num):
+            order_list = symbol_order_params[i:i + num]
+            params = {'batchOrders': exchange.json(order_list),
+                      'timestamp': int(time.time() * 1000)}
+            order_info = exchange.fapiPrivatePostBatchOrders(params)
+            print('\n成交订单信息\n', order_info)
+
+        # 发送钉钉
+        dingding_report_every_loop(symbol_info, symbol_signal, order_list, run_time, robot_id_secret)
+
+        # 本次循环结束
+        print('\n', '-' * 40, '本次循环结束，%d秒后进入下一次循环' % long_sleep_time, '-' * 40, '\n\n')
+        time.sleep(long_sleep_time)
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            main()
+        except Exception as e:
+            content = '系统出错，10s之后重新运行，出错原因：' + str(e)
+            print(content)
+            send_dingding_msg(content, robot_id=robot_id_secret[0], secret=robot_id_secret[1])
+            time.sleep(long_sleep_time)
